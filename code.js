9d222c6bc551051cad938bc843f1638f41fda053
Joel Chambers <104284414+Boomboomyoyo@users.noreply.github.com>
2023-11-08 16:52:17 -0700

    Added brainstorming notes from whiteboard

---
+++ code.js
@@ -0,0 +1,10 @@
+/* 
+V nodes
+between V-1 and 2V edges
+connectedGraph(Int, Double): List[List[Int]]
+graph = []
+for(i=0; i< V; i++)
+    graph.push([])
+edges = Math.floor((v-1xEdgeRatio))
+connectedGraph2(graph, edges, connectedNodes=[])
+*/
